267a1e790ff8a83e7e61934f62beaea2dfe7ce31
Cameron Cundiff <ckundo@gmail.com>
2013-08-25 19:59:49 -0400

    Support HTML Code Sniffer as a runner option.

--- lib/access_lint/audit.rb
+++ lib/access_lint/audit.rb
@@ -8,16 +8,22 @@ module AccessLint
     end
 
     def run(rule_set_name)
-      `#{runner_command_for(rule_set_name)} #{@target}`
+      `#{command_for(rule_set_name)}`
     end
 
     private
 
-    def runner_command_for(rule_set_name)
+    def command_for(rule_set_name)
       configuration = YAML.load_file(
         File.expand_path('../../../config/rule_sets.yml', __FILE__)
       )
-      configuration['rule_sets'].fetch(rule_set_name.to_s)['runner_command']
+      runner = configuration['rule_sets'].fetch(rule_set_name.to_s)['runner']
+      directory = runner.fetch('directory')
+      path = File.expand_path(runner.fetch('filename'), directory)
+      arguments = runner.fetch('additional_arguments') || {}
+      arguments = arguments.values.any? ? arguments.values.join(' ') : ''
+
+      "cd #{directory} && phantomjs #{path} #{@target} #{arguments}"
     end
   end
 end
