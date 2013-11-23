0e7a94e25582f89b9884d8311ac0f46da124bab6
Cameron Cundiff <dev@ckundo.com>
2013-11-23 17:39:08 -0500

    Return error results as individual objects.

--- vendor/access-lint/bin/auditor.js
+++ vendor/access-lint/bin/auditor.js
@@ -10,17 +10,40 @@ if (system.args.length !== 2) {
   page.open(url, function (status) {
     if (status === 'success') {
       page.injectJs('../../google-chrome/accessibility-developer-tools/gen/axs_testing.js');
+
       var report = page.evaluate(function() {
-        return axs.Audit.run();
+        var raw_results = axs.Audit.run();
+        var results = [];
+
+        for(var i = 0; i < raw_results.length; i++) {
+          var element_names = [];
+          var elements = raw_results[i]['elements'];
+
+          if(elements !== undefined) {
+            for(var j = 0; j < elements.length; j++) {
+              var element = elements[j];
+              var tmp = document.createElement("div");
+              tmp.appendChild(element);
+              element_names.push(tmp.innerHTML);
+            }
+          }
+
+          results.push({
+            status: raw_results[i]['result'],
+            title: raw_results[i]['rule']['heading'],
+            severity: raw_results[i]['rule']['severity'],
+            element_names: element_names,
+            elements: raw_results[i]['elements']
+          });
+        }
+
+        return results;
       });
+
       console.log(JSON.stringify(report));
       phantom.exit();
     } else {
-      console.log('Failed to load the page at ' +
-        url +
-        ". Status was: " +
-        status
-        );
+      console.log('Failed to load the page at ' + url + ".");
       phantom.exit();
     }
   });
