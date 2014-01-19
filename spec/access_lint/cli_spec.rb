32418e69235669518b1028610825ad624b60dbf3
Cameron Cundiff <dev@ckundo.com>
2014-01-19 11:40:01 -0500

    Pass correct arguments to CLI.start in spec.

--- spec/access_lint/cli_spec.rb
+++ spec/access_lint/cli_spec.rb
@@ -4,7 +4,7 @@ require 'access_lint/cli'
 module AccessLint
   describe CLI do
     it 'creates an audit' do
-      expect { CLI.start }.to_not raise_error
+      expect { CLI.start([]) }.to_not raise_error
     end
   end
 end
