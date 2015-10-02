e51b4530e0e2c668f951872bcf79708a295a9a9d
Cameron Cundiff <github@ckundo.com>
2015-10-01 20:22:27 -0400

    Update rules, print more terse output, exit 1 on failures.

--- lib/access_lint/cli.rb
+++ lib/access_lint/cli.rb
@@ -8,9 +8,43 @@ module AccessLint
 
     desc 'audit', 'audit TARGET'
     def audit(target)
+      exit_code = 0
+
       report(message: "auditing #{target}", color: "blue") do
-        ap Audit.new(target).run, index: false
+        results = Audit.new(target).run
+
+        results.each do |group|
+          section = group[0]
+
+          case section
+          when "NA"
+            color = :blue
+          when "PASS"
+            color = :green
+          when "FAIL"
+            color = :red
+          end
+
+          group[1].each do |rule|
+            name = rule.fetch("title")
+            severity = rule.fetch("severity")
+            elements = rule.fetch("element_names")
+
+            puts
+            aligned("#{section} - #{name} (#{severity})", color: color)
+
+            elements.each do |element|
+              aligned("* #{element[0..80]}...")
+            end
+
+            if section == "FAIL" && elements.any?
+              exit_code = 1
+            end
+          end
+        end
       end
+
+      exit exit_code
     end
   end
 end
